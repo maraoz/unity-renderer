bcdc9859067eaea78d808cccfc93972d67f47e73
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-11-01 03:41:20 -0500

    pre-run version

--- unity-renderer/Assets/Scripts/MapperCamera.cs
+++ unity-renderer/Assets/Scripts/MapperCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DCL.Controllers;
 
 public class MapperCamera : MonoBehaviour
 {
@@ -20,6 +21,10 @@ public class MapperCamera : MonoBehaviour
 
         transform.position = target.position + new Vector3(1,1,1) * distance;
     }
+
+    private bool showResolution = false;
+    private bool started = false;
+
     void Update()
     {
 
@@ -35,32 +40,47 @@ public class MapperCamera : MonoBehaviour
         }
 
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !started)
         {
             target.position += new Vector3(parcelSize/2, 0, parcelSize/2);
             GameObject debugView = GameObject.Find("DebugView(Clone)");
             debugView.SetActive(false);
             avatarRenderer.SetActive(false);
             GameObject.Find("_ExploreV2").SetActive(false);
-            MapperTick();
+
+            started = true;
+
+            // go to first position
+            DCLCharacterController.i.SetPosition(new Vector3(parcelSize/2, 0, parcelSize/2));
+            WaitForScreenshot();
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad4))
+        // print screen resolution if P is pressed
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            target.position += new Vector3(-parcelSize, 0, 0);
+            showResolution = !showResolution;
         }
-        if (Input.GetKeyDown(KeyCode.Keypad6))
-        {
-            target.position += new Vector3(parcelSize, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad8))
-        {
-            target.position += new Vector3(0, 0, parcelSize);
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad5))
-        {
-            target.position += new Vector3(0, 0, -parcelSize);
-        }
+        if (showResolution)
+            Debug.Log("Screen resolution: " + Screen.width + "x" + Screen.height);
+
+
+        // manual controls
+        // if (Input.GetKeyDown(KeyCode.Keypad4))
+        // {
+        //     target.position += new Vector3(-parcelSize, 0, 0);
+        // }
+        // if (Input.GetKeyDown(KeyCode.Keypad6))
+        // {
+        //     target.position += new Vector3(parcelSize, 0, 0);
+        // }
+        // if (Input.GetKeyDown(KeyCode.Keypad8))
+        // {
+        //     target.position += new Vector3(0, 0, parcelSize);
+        // }
+        // if (Input.GetKeyDown(KeyCode.Keypad5))
+        // {
+        //     target.position += new Vector3(0, 0, -parcelSize);
+        // }
         
         transform.position = target.position + new Vector3(0,1,0) * distance;
         transform.LookAt(target);
@@ -71,12 +91,8 @@ public class MapperCamera : MonoBehaviour
     private Vector3 currentPosition;
     private int layer = 1;
     private int leg = 0;
-    void MapperTick()
+    void GoToNextParcel()
     {
-        Debug.Log("currentX: " + currentX + " currentY: " + currentY + " layer: " + layer + " leg: " + leg + " currentPosition: " + currentPosition);
-        // move player to current position
-        DCLCharacterController.i.SetPosition(new Vector3(parcelSize/2, 0, parcelSize/2) + currentPosition);
-
         // calculate next position
         // spiral around (0,0) through the grid
         // layer 0 -> (0,0)
@@ -92,7 +108,45 @@ public class MapperCamera : MonoBehaviour
         }
         currentPosition = new Vector3(currentX*N*parcelSize, 0, currentY*N*parcelSize);
 
-        Invoke("MapperTick", 5f);
+        Debug.Log("currentX: " + currentX + " currentY: " + currentY + " layer: " + layer + " leg: " + leg + " currentPosition: " + currentPosition);
+        // move player to current position
+        DCLCharacterController.i.SetPosition(new Vector3(parcelSize/2, 0, parcelSize/2) + currentPosition);
+
+        Invoke("WaitForScreenshot", 3f);
+    }
+
+    void WaitForScreenshot()
+    {
+        string desktopPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+        string name = currentX*N + "," + currentY*N + ".png";
+        string fullScreenshotPath = desktopPath + "/map/" + name;
+        // check if screenshot was already taken
+        if (System.IO.File.Exists(fullScreenshotPath))
+        {
+            Debug.Log("Screenshot already exists for coordinate (" + currentX*N + "," + currentY*N + ")");
+            GoToNextParcel();
+            return;
+        }
+
+        // check if all scenes are loaded
+        bool ready = true;
+        ParcelScene[] scenes = FindObjectsOfType<ParcelScene>();
+        foreach (ParcelScene scene in scenes)
+        {
+            if (!scene.gameObject.name.Contains("ready!"))
+                ready = false;
+        }
+
+        if (ready) {
+            Debug.Log("all scenes are loaded, taking screenshot at (" + currentX*N + "," + currentY*N + ")");
+            ScreenCapture.CaptureScreenshot(fullScreenshotPath);
+            Invoke("GoToNextParcel", 2f);
+        }
+        else {
+            Debug.Log("waiting for screenshot at (" + currentX*N + "," + currentY*N + ")");
+            Invoke("WaitForScreenshot", 2f);
+        }
+
     }
 
 }
